0713c18f0cd05e4f4acd7f790080a84709183c9f
Raphael Cerqueira <raphaelfc@gmail.com>
2021-11-14 15:22:08 -0300

    Initial Commit

---
+++ TelegramRedesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TelegramRedesign
+//
+//  Created by Raphael Cerqueira on 14/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TelegramRedesign/TelegramRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramRedesignApp.swift
+//  TelegramRedesign
+//
+//  Created by Raphael Cerqueira on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TelegramRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
